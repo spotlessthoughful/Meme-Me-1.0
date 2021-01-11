c5f9e650c6ed2a6a8989ba3354afdd4af670018f
Nate Lema <nate.lema@candidpartners.com>
2021-01-10 23:23:56 -0500

    Initial Commit

---
+++ Meme Me 1.0/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Meme Me 1.0
+//
+//  Created by Nathaniel Lema on 1/10/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
