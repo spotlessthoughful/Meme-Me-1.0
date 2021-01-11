aef7f831e45a4166920bbbeb63d5029275e53855
Nate Lema <nate.lema@candidpartners.com>
2021-01-11 00:49:59 -0500

    Added Album IBAction And Image View IBOutlet

--- Meme Me 1.0/ViewController.swift
+++ Meme Me 1.0/ViewController.swift
@@ -9,11 +9,20 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    // MARK: IBOutlets
+    @IBOutlet weak var imagePickerView: UIImageView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
-
+    
+    // MARK: IBActions
+    @IBAction func pickAlbumImage(_ sender: Any) {
+        let pickerController = UIImagePickerController()
+        present(pickerController, animated: true, completion: nil)
+    }
+    
 
 }
 
